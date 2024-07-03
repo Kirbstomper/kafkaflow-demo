94b8f3313e9b7f38e175ac9104c5d95016b8c14e
Chris Smith <chris.smith@ahead.com>
2024-07-03 13:03:02 -0400

    Add producer

--- producer/Program.cs
+++ producer/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using KafkaFlow;
+using KafkaFlow.Producers;
+using KafkaFlow.Serializer;
+using Microsoft.Extensions.DependencyInjection;
+using Producer;
+
+var services = new ServiceCollection();
+
+const string topicName = "sample-topic";
+const string producerName = "say-hello";
+const string unstructuredTopicName = "custom-topic";
+
+services.AddKafka(kafka =>
+    kafka
+        .UseConsoleLog()
+        .AddCluster(cluster =>
+            cluster
+                .WithBrokers(new[] { "localhost:9092" })
+                .CreateTopicIfNotExists(topicName, 1, 1)
+                .CreateTopicIfNotExists(unstructuredTopicName, 1, 1)
+                .AddProducer(
+                    producerName,
+                    producer =>
+                        producer
+                            .DefaultTopic(topicName)
+                            .AddMiddlewares(m => m.AddSerializer<JsonCoreSerializer>())
+                )
+        )
+);
+
+var serviceProvider = services.BuildServiceProvider();
+
+var producer = serviceProvider.GetRequiredService<IProducerAccessor>().GetProducer(producerName);
+
+await producer.ProduceAsync(
+    topicName,
+    Guid.NewGuid().ToString(),
+    new HelloMessage { Text = "Hello!" }
+);
+
+Console.WriteLine("Message sent!");
---
+++ producer/src/CustomMessage.cs
@@ -0,0 +1,10 @@
+namespace Producer;
+
+public class CustomMessage
+{
+    public string Title { get; set; } = default!;
+
+    public string Content { get; set; } = default!;
+
+    public string ProducerOnly { get; set; } = default!;
+}
